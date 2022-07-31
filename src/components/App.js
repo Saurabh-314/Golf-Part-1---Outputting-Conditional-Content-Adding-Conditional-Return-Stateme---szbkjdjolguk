7dbf705eb9067c2540b54bbe72e460a10eb8192c
Saurabh-314 <maxxxjob314@gmail.com>
2022-07-31 10:35:57 +0530

    fix it

--- src/components/App.js
+++ src/components/App.js
@@ -14,8 +14,7 @@ class App extends Component {
     };
 
     buttonClickHandler() {
-        console.log("button clicked");
-        this.setState({renderBall : true});
+    this.setState({renderBall : true});
    }
     renderBallOrButton() {
 		if (this.state.renderBall) {
@@ -29,10 +28,10 @@ class App extends Component {
     componentDidMount() {
         document.addEventListener('keydown',(e)=>{
             let newPosi = this.state.posi + 5;
-            if(e.key === 'ArrowRight'){
+            if(e.key === "ArrowRight"){
                 this.setState({
                     posi : newPosi,
-                    ballPosition: { left: `${this.state.posi}px` }
+                    ballPosition: { left: `${newPosi}px` }
                 })
             }
         })
